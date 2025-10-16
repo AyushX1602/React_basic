b310c70d54cdf621e41abf076b524a09f483e5c7
AyushX1602 <ayushpathak16022005.com>
2025-10-16 10:52:01 +0530

    Update todo app: fix input elements and improve component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import React from "react";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App(){
+  const [todos,settods] = useState([{
+    title : "go to gym",
+    description : "go daily",
+    done : false
+  }]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function addTodo(){
+  let Array=[];
+  for(let i=0;i<todos.length;i++){
+    Array.push(todos[i]);
+  }
+    Array.push({
+    title : document.getElementById("title").value,
+    description: document.getElementById("description").value,
+    done: true,
+  });
+  settods(Array);
 }
 
-export default App
+  return (
+    <div>
+      <input id="title" type="text" placeholder="Title"/>
+       <input id="description" type="text" placeholder="Description"/>
+      <button onClick={addTodo}>ADD TODO </button>
+      <br />
+      {JSON.stringify(todos)}
+    </div>
+    
+   
+  );
+}
