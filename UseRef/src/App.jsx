d352c748ec4f0e492f92d815ca3bd1d4acc4f46e
AyushX1602 <ayushpathak16022005@gmail.com>
2025-10-25 00:51:00 +0530

    Add useFetch hook for data fetching
    - Implemented a custom hook `useFetch` to handle API requests.
    - The hook accepts a URL and fetches data from it, storing the result in state.
    - Utilizes `useEffect` to trigger data fetching when the URL changes.
    - Returns the fetched data for use in components.
    - Added a PNG image file to the public directory.

--- UseRef/src/App.jsx
+++ UseRef/src/App.jsx
@@ -1,18 +1,72 @@
-import { useRef , useState } from "react"
-import './App.css'
+import { useRef, useState, createContext, useContext } from "react";
+import './App.css';
+import { useFetch } from "./hooks/useFetch";
 
-function App(){
-const inputRef = useRef();
+// const BulbContext = createContext();
 
-function focusonsubmit(){
-  inputRef.current.focus();
+// function BulbProvider({ children }) {
+//    const [bulbOn,setBulbOn] = useState(true);
+//    return <BulbContext.Provider value={{
+//     bulbOn : bulbOn,
+//     setBulbOn : setBulbOn
+//    }}>
+//     {children}
+//    </BulbContext.Provider>
+// }
+
+// function App(){
+ 
+//   return <div>
+// <BulbProvider>
+//   <LightBulb />
+//   <ToggleLightState /> 
+// </BulbProvider>
+
+//   </div>
+// }
+
+// function Light(){
+//   // const[bulbOn,setBulbOn] = useState(true);
+//   return <div>
+//     <LightBulb />
+//     <ToggleLightState />
+//     {/* <LightBulb bulbOn = {bulbOn} />
+//     <ToggleLightState setBulbOn = {setBulbOn} /> */}
+//   </div>
+// }
+
+// function LightBulb(){
+//   const {bulbOn} = useContext(BulbContext);
+//  return <div>
+//   <img
+//     src={bulbOn ? "Bulbon.png" : "Bulboff.png"}
+//     alt={bulbOn ? "Bulb On":"Bulb off"}
+//     style={{ width: "100px", height: "180px" }}
+//   />
+//   <br />
+//   {bulbOn ? "Bulb On":"Bulb off"}
+//  </div>
+// }
+
+// function ToggleLightState() {
+//   const {bulbOn,setBulbOn} = useContext(BulbContext);
+//   function toggleBulb(){
+//     setBulbOn((bulbOn) => !bulbOn)
+//   }
+//   return <div>
+//     <button onClick={toggleBulb}>Toggle Bulb</button>
+//   </div>
+// }
+
+
+function App() {
+  const {finaldata} = useFetch("https://jsonplaceholder.typicode.com/todos/1");
+
+  return (
+    <div>
+      {JSON.stringify(finaldata)}
+    </div>
+  )
 }
-  return <div>
-    signup form
-    <input ref={inputRef} type="text"></input>
-    <input type="text"></input>
-    <button onClick={focusonsubmit}>submit</button>
-  </div>
-}
 
-export default App
+export default App;
---
+++ UseRef/src/hooks/useFetch.js
@@ -0,0 +1,20 @@
+import { use, useEffect,useState } from "react";
+
+
+export function useFetch(url) {
+const [finaldata , setFinaldata] = useState ({});
+
+
+async function getDetails() {
+    const response = await fetch (url);
+    const json = await response.json();
+    setFinaldata(json);
+}
+
+useEffect(()=> {
+    getDetails ();
+},[url]);
+
+return {finaldata};
+
+}
